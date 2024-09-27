43085c54252998a79708963d2f28e49e6d0ea5f4
SayedTahsin <mail.tahsin99@gmail.com>
2024-09-27 20:00:22 +0600

    Wrap up
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,30 @@ mongoConfig();
 app.use('/api/user', authRoutes);
 app.use('/api/tasks', checkAuth, taskRoutes);
 app.use('/api/notes', checkAuth, noteRoutes);
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Task App</title>
+        <style>
+          body {
+            height: 100vh;
+            margin: 60;
+            text-align: center;
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            color: #333;
+          }
+        </style>
+      </head>
+      <body>
+        <h1><code>Assalamualykum</code></h1>
+        <br>
+        <code>I have decided to re-write this app using Hono and Bun.</code>
+      </body>
+    </html>
+  `);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running in port: ${PORT}`);
